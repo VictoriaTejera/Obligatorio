6a2388c23eacded8d6068b04393766268f65c2d5
VictoriaTejera <victoriatejera15@gmail.com>
2018-05-31 10:19:31 -0300

    Actualizacion2

--- Obligatorio/src/Producto.java
+++ Obligatorio/src/Producto.java
@@ -1,8 +1,14 @@
 
+
 public class Producto {
 
 	private String nombre;
 	private String nombreFantasia;
 	private String estado;
+	private Clase clase;
+	private Pais pais;
+	private Marca marca;
+	private Empresa empresa;
+	
 	
 }
