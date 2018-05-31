d4b8efa31f792670ba77aa795697cec9ee4b659d
VictoriaTejera <victoriatejera15@gmail.com>
2018-05-31 18:42:52 -0300

    Hash

---
+++ grupo7-p2-tads/src/uy/edu/um/prog2/adt/Hash/ClaveInvalida.java
@@ -0,0 +1,15 @@
+package uy.edu.um.prog2.adt.Hash;
+
+public class ClaveInvalida extends Exception {
+
+	public ClaveInvalida() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClaveInvalida(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ grupo7-p2-tads/src/uy/edu/um/prog2/adt/Hash/NodoHash.java
@@ -0,0 +1,49 @@
+package uy.edu.um.prog2.adt.Hash;
+
+
+public class NodoHash<K,T> {
+
+		private K clave;
+		private T valor;
+		private boolean eliminado;
+		
+		public NodoHash(K clave, T valor) {
+			super();
+			this.clave = clave;
+			this.valor = valor;
+			eliminado=false;
+		}
+
+
+		public boolean isEliminado() {
+			return eliminado;
+		}
+
+
+		public void setEliminado(boolean eliminado) {
+			this.eliminado = eliminado;
+		}
+
+
+		public K getClave() {
+			return clave;
+		}
+
+
+		public void setClave(K clave) {
+			this.clave = clave;
+		}
+
+
+		public T getValor() {
+			return valor;
+		}
+
+
+		public void setValor(T valor) {
+			this.valor = valor;
+		}
+		
+		
+		
+}
