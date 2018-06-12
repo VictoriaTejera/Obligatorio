9e06d4d0b503f9973ab0bd90b232ac49b321da10
PazFraschini <pazfraschini@gmail.com>
2018-06-11 21:04:05 -0300

    ANDA EL REPORTE2!!!!!!!

--- grupo7-p2-tads/src/uy/edu/um/prog2/adt/Iterator/MyIterator.java
+++ grupo7-p2-tads/src/uy/edu/um/prog2/adt/Iterator/MyIterator.java
@@ -6,7 +6,7 @@ import uy.edu.um.prog2.adt.Hash.NodoHash;
 
 
 
-public class MyIterator<K,T> implements Iterator<T> {
+/*public class MyIterator<K,T> implements Iterator<T> {
 
 	protected int posicion = 0;
 	protected NodoHash<K, T>[] vectorHash;
@@ -56,6 +56,47 @@ public class MyIterator<K,T> implements Iterator<T> {
 			
 		}
 		return oExit;
+	}
+
+}*/
+public class MyIterator<K, T> implements Iterator<T> {
+	private NodoHash<K, T>[] vector;
+	private int posicion = 0;
+
+	public MyIterator(NodoHash<K,T>[] vector) {
+		this.vector = vector;
+
+		while ((vector[posicion] == null || vector[posicion].isEliminado()) && posicion < vector.length - 1) {
+			posicion++;
+		}
+		if(posicion==vector.length-1 && (vector[posicion] == null || vector[posicion].isEliminado())) {
+			posicion=0;
+		}
+	}
+
+	public boolean hasNext() {
+		boolean hasNext = false;
+		int posAux = posicion;
+		while (!hasNext && posAux < vector.length) {
+			if (vector[posAux] != null && !vector[posAux].isEliminado()) {
+				hasNext = true;
+			}
+			posAux++;
+		}
+		return hasNext;
+	}
+
+	public T next() {
+		T next = null;
+		int posAux = posicion;
+		while (next == null && posAux < vector.length) {
+			if (vector[posAux] != null && !vector[posAux].isEliminado()) {
+				next = vector[posAux].getValor();
+			}
+			posAux++;
+		}
+		posicion = posAux;
+		return next;
 	}
 
 }
