7bc9008a44afc9a8dab455747fd0d3987ccbce15
PazFraschini <pazfraschini@gmail.com>
2018-06-11 21:16:49 -0300

    cuenta mal..

--- grupo7-p2-tads/src/uy/edu/um/prog2/adt/Iterator/MyIterator.java
+++ grupo7-p2-tads/src/uy/edu/um/prog2/adt/Iterator/MyIterator.java
@@ -59,7 +59,7 @@ import uy.edu.um.prog2.adt.Hash.NodoHash;
 	}
 
 }*/
-public class MyIterator<K, T> implements Iterator<T> {
+	/*public class MyIterator<K, T> implements Iterator<T> {
 	private NodoHash<K, T>[] vector;
 	private int posicion = 0;
 
@@ -99,4 +99,57 @@ public class MyIterator<K, T> implements Iterator<T> {
 		return next;
 	}
 
-}
+}*/
+	public class MyIterator<K,T> implements Iterator<T> {
+
+		protected int posicion = 0;
+		protected NodoHash<K, T>[] vectorHash;
+
+		public MyIterator(NodoHash<K,T>[] vector) {
+			
+			vectorHash=vector;
+			
+			while ((vector[posicion] == null || vector[posicion].isEliminado()) && posicion < vector.length - 1) {
+				posicion++;
+			}
+			if(posicion==vector.length-1 && (vector[posicion] == null || vector[posicion].isEliminado())) {
+				posicion=0;
+			}
+		}
+
+		
+		public boolean hasNext() {
+			
+			boolean bExit=false;
+			
+			int count =posicion;
+			 while(!bExit && count< vectorHash.length) {
+				 
+				 if(vectorHash[count]!=null && !vectorHash[count].isEliminado()) {
+					 bExit = true;
+				 }
+				 count++;
+			 }
+			
+			return bExit;
+		}
+
+
+		public T next() {
+			
+			T	oExit=null;
+		
+			
+			while( oExit == null && posicion<vectorHash.length) {
+				
+				if(vectorHash[posicion]!=null && !vectorHash[posicion].isEliminado()) {
+					oExit=vectorHash[posicion].getValor();
+				}
+				posicion++;
+				
+			}
+			return oExit;
+		}
+
+	}
+  
